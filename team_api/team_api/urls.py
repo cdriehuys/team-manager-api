09e9c9f5b1e8bea74155f1b48031c49f260dc534
Chathan Driehuys <cdriehuys@gmail.com>
2017-06-05 15:28:22 -0400

    Remove autogenerated docs.
    The autogenerated docs aren't currently working, and they aren't used.
    We can investigate better methods of including docs later when we need
    it.
    
    Fixes #2

--- team_api/team_api/urls.py
+++ team_api/team_api/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from rest_framework.documentation import include_docs_urls
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),     # noqa
     url(r'^auth/', include('rest_auth.urls')),
-    url(r'^docs/', include_docs_urls('Team Manager API')),
     url(r'^team/', include('teams.urls')),
 ]
