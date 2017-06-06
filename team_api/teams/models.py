11fb130f9d3fea658c57d504e8f4fe6fad113aa4
Chathan Driehuys <cdriehuys@gmail.com>
2017-06-05 22:47:06 -0400

    Add method to send invite notification email.

--- team_api/teams/models.py
+++ team_api/teams/models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core import mail
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 
 class Team(models.Model):
@@ -30,6 +33,44 @@ class TeamInvite(models.Model):
         'teams.Team',
         on_delete=models.CASCADE,
         verbose_name=_('team'))
+
+    @property
+    def user_exists(self):
+        """
+        Determine if there is a user with the invite's email.
+        """
+        return get_user_model().objects.filter(email=self.email).exists()
+
+    def send_notification(self):
+        """
+        Send an email notification about the invite.
+
+        Returns:
+            ``True`` if the email was successfully sent and ``False``
+            otherwise.
+        """
+        subject = ugettext('Invited to Join %(team)s' % {
+            'team': self.team.name
+        })
+        message = render_to_string('teams/email/invite.txt')
+
+        return bool(mail.send_mail(
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            message=message,
+            recipient_list=[self.email],
+            subject=subject))
+
+    def _get_notification_context(self):
+        """
+        Get context data to use when sending a notification email.
+
+        Returns:
+            A dictionary containing context for the notification email.
+        """
+        return {
+            'invite': self,
+            'team': self.team,
+        }
 
 
 class TeamMember(models.Model):
