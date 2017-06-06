98102eccd9332007ee117cd83dd58b49cfed6d61
Chathan Driehuys <cdriehuys@gmail.com>
2017-06-06 16:29:39 -0400

    Ensure team members are unique.
    There cannot be two team members with the same team/user combination.

--- team_api/teams/tests/models/test_team_member_model.py
+++ team_api/teams/tests/models/test_team_member_model.py
@@ -1,3 +1,7 @@
+from django.db.utils import IntegrityError
+
+import pytest
+
 from teams.models import TeamMember
 
 
@@ -10,3 +14,15 @@ def test_create(team_factory, user_factory):
         member_type=TeamMember.PLAYER,
         team=team_factory(),
         user=user_factory())
+
+
+def test_unique_team_and_user(team_member_factory):
+    """
+    Team members should be unique with respect to user and team.
+
+    Regression test for #14
+    """
+    member = team_member_factory()
+
+    with pytest.raises(IntegrityError):
+        team_member_factory(team=member.team, user=member.user)
